3349758333ecd40a72736ffca9ce255a9bd1673d
acd02 <alexandre.cadiot@adevinta.com>
2023-03-31 15:12:53 +0200

    feat: add support for rem values with varying base HTML font sizes

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,27 @@
 export const removeDefaultSuffix = str => str.replace(/-(default|DEFAULT)/, '')
 
+const getRemEquivalentValue = (remValue, htmlFontSize) => {
+  const defaultBrowserBase = 16
+  const pxValue = parseFloat(remValue) * defaultBrowserBase
+  return `${pxValue / htmlFontSize}rem`
+}
+
 export const remToPx = (rem, config) => {
   // if non rem value passed in return value as is
   if (typeof rem === 'string' && rem.search('rem') === -1) {
     return parseFloat(rem)
   }
 
-  return parseFloat(rem) * config.theme.configViewer.baseFontSize
+  const defaultBrowserBase = 16
+  const pxValue = parseFloat(rem) * defaultBrowserBase
+
+  return pxValue
 }
 
 export const appendPxToRems = (rem, config) => {
   if (rem.search('rem') === -1) return rem
 
   return rem.replaceAll(/(\d|\.)*rem/g, (value) => {
-    return `${value} (${remToPx(value, config)}px)`
+    return `${getRemEquivalentValue(value, config.theme.configViewer.baseFontSize)} (${remToPx(value, config)}px)`
   })
 }
